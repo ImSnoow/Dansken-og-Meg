28f4eb184caf9d4edb18e91bef8450052fb87e20
ImSnoow <84009109+ImSnoow@users.noreply.github.com>
2021-08-18 10:21:37 +0200

    Added model

---
+++ Model.js
@@ -0,0 +1,26 @@
+const model = {
+
+    //app-nivå ting
+    app: {
+        currentpage: "Home"
+
+    }
+
+    // inp utfelter til hver side
+
+
+
+
+    // felles data
+
+
+
+
+    /*
+    To spørsmål for hver side:
+    
+    1. Hvilke data trenger vi i modellen for å kunne vise dette skjermbildet? (query)
+    
+    2. Hva kan man gjøre på dette skjermbildet - og hvordan skal den endringen lagres i modellen? (command)
+    */
+}
