ffabfb78813459949bec6d62388aba9b632053a4
ImSnoow <84009109+ImSnoow@users.noreply.github.com>
2021-08-18 13:53:48 +0200

    Update Model.js
    La til fler underkategorier

--- Model.js
+++ Model.js
@@ -14,19 +14,30 @@ const model ={
       {id: 1, name: 'møbler'},
       {id: 2, name: 'stoler', parentId: 1 },
       {id: 3, name: 'bord', parentId: 1 },
-
+      {id: 4, name: 'kommoder', parentId: 1 },
+      {id: 5, name: 'hyller', parentId: 1 },
   ],
   categoriesClothes: [
       {id: 1, name: 'klær og tilbehør'},
       {id: 2, name: 'kjoler', parentId: 1 },
       {id: 3, name: 'bukser', parentId: 1 },
+      {id: 4, name: 'skjørt', parentId: 1 },
+      {id: 5, name: 'jakker', parentId: 1 },
+      {id: 6, name: 'kåper', parentId: 1 },
+      {id: 7, name: 'sko', parentId: 1 },
+      {id: 8, name: 'vesker', parentId: 1 },
+      {id: 9, name: 'smykker', parentId: 1 },
 
   ],
 
   categoriesKnicknacks: [
     {id: 1, name: 'Småting'},
-    {id: 2, name: 'skåler', parentId: 1 },
-    {id: 3, name: '', parentId: 1 },
+    {id: 2, name: 'boller', parentId: 1 },
+    {id: 3, name: 'vaser', parentId: 1 },
+    {id: 4, name: 'lamper', parentId: 1 },
+    {id: 5, name: 'servise', parentId: 1 },
+    {id: 6, name: 'krukker', parentId: 1 },
+    {id: 7, name: 'fat', parentId: 1 },
 
 ],
   
