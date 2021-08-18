a0791bf1ae98f91350636c48900f787c384928b2
stefeanos <84009599+stefeanos@users.noreply.github.com>
2021-08-18 13:13:05 +0200

    Update Model.js

--- Model.js
+++ Model.js
@@ -13,61 +13,7 @@ const model ={
 
 };
 
-let products = {
-    120: {
-    name: '',
-    desc: '',
-    img: '',
-    price: 20000
-  },
-     121: {
-    name: '',
-    desc: '',
-    img: '',
-    price: 20000
-  },
-     122: {
-    name: '',
-    desc: '',
-    img: '',
-    price: 20000
-  },
-     123: {
-    name: '',
-    desc: '',
-    img: '',
-    price: 20000
-  },
-     124: {
-    name: '',
-    desc: '',
-    img: '',
-    price: 20000
-  },
-     125: {
-    name: '',
-    desc: '',
-    img: '',
-    price: 20000
-  },
-     126: {
-    name: '',
-    desc: '',
-    img: '',
-    price: 20000
-  },
-     127: {
-    name: '',
-    desc: '',
-    img: '',
-    price: 20000
-  },
-     128: {
-    name: '',
-    desc: '',
-    img: '',
-    price: 20000
-  },
+
   
   /*
 To spørsmål for hver side:
